3643d5bf8387426b350fdd17f9bbdfbcb4a999fc
Matei Dragu <1160952+matei-dragu@users.noreply.github.com>
2015-10-11 21:33:31 +0200

    Formatting

--- angular-js-learn/angulartest.js
+++ angular-js-learn/angulartest.js
@@ -1,18 +1,24 @@
 var phonecatApp = angular.module('phonecatApp', []);
 
-phonecatApp.controller('PhoneListCtrl', function ($scope) {
-  $scope.phones = [
-    {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.'},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'},
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'}
-  ];
-  
-  $scope.name = "Pls2";
-  
-  $scope.gob = function() {
-      return "GOOOBIIEE6";
-  }
+phonecatApp.controller('PhoneListCtrl', function($scope) {
+
+    console.info("PhoneListCtrl Constructor")
+    console.info($scope)
+    
+    $scope.phones = [ {
+        'name' : 'Nexus S',
+        'snippet' : 'Fast just got faster with Nexus S.'
+    }, {
+        'name' : 'Motorola XOOM™ with Wi-Fi',
+        'snippet' : 'The Next, Next Generation tablet.'
+    }, {
+        'name' : 'MOTOROLA XOOM™',
+        'snippet' : 'The Next, Next Generation tablet.'
+    } ];
+
+    $scope.name = "Pls2";
+
+    $scope.gob = function() {
+        return "GOOOBIIEE6";
+    }
 });
